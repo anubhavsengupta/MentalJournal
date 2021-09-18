a59e1edd1304332af88b9cc5d002604fdf70605e
anubhavsengupta <34662860+anubhavsengupta@users.noreply.github.com>
2021-09-18 11:42:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+import secrets
+from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
