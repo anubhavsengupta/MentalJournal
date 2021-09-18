a65263b04d6dffca0d21e3d7e7c173807c9af383
Talha Ahmad <talha05ahmad@gmail.com>
2021-09-18 13:03:13 -0400

    Just some more updates

--- app.py
+++ app.py
@@ -1,18 +1,37 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
-import secrets
-from datetime import timedelta
+from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy import text, create_engine
+from secrets import token_urlsafe
 
 app = Flask(__name__)
 engine = create_engine('sqlite+pysqlite:///database.db')
 
+app.secret_key = token_urlsafe()
+
 @app.route('/')
 def index():
 	return render_template('index.html')
+	
 
-@app.route("/login")
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-	return render_template("login.html")
+	if request.method == 'POST':
+		flash('hello')
+		#username = request.form.get('username')
+		#password = request.form.get('password')
+		return 'hello'
+
+	elif request.method == 'GET':
+		return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	if request.method == 'POST':
+		username = request.form.get('username')
+		password = request.form.get('password')
+
+	elif request.method == 'GET':
+		return render_template('register.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
