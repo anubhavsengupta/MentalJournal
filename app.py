868ca794e0012f5badf6a4178ad6a5306b43a160
Talha Ahmad <talha05ahmad@gmail.com>
2021-09-18 11:57:47 -0400

    Updated App.py and Setup Virtual Environment

--- app.py
+++ app.py
@@ -4,3 +4,10 @@ from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+if __name__ == '__main__':
+	app.run(debug=True)
