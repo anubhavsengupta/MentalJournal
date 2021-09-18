b64afc27afffc226a5ade00b2266efa9c1e7eba4
Talha Ahmad <talha05ahmad@gmail.com>
2021-09-18 12:48:56 -0400

    Nothing important

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 import secrets
 from datetime import timedelta
-from sqlalchemy import engine
+from sqlalchemy import text, create_engine
 
 app = Flask(__name__)
+engine = create_engine('sqlite+pysqlite:///database.db')
 
 @app.route('/')
 def index():
